a78ae9dc7e4ee98591f5288af8dd0cdfece6dcd1
Steve Crawshaw <murrayjarvis@gmail.com>
2026-03-24 16:00:30 +0000

    added helper db connect and load pins functions

---
+++ scripts/R/db_connect.R
@@ -0,0 +1,15 @@
+library(DBI)
+library(RPostgres)
+readRenviron(".env")
+
+con <- dbConnect(
+    Postgres(),
+    host = Sys.getenv("POSTGRES_HOST"),
+    port = as.integer(Sys.getenv("POSTGRES_PORT")),
+    dbname = Sys.getenv("POSTGRES_DB"),
+    user = Sys.getenv("POSTGRES_USER"),
+    password = Sys.getenv("POSTGRES_PASSWORD")
+)
+
+# Test connection
+dbGetQuery(con, "SELECT current_database(), current_user, version()")
---
+++ scripts/R/load_pins.R
@@ -0,0 +1,24 @@
+library(pins)
+library(arrow)
+library(tidyverse)
+library(sf)
+
+board <- board_s3(
+    bucket = "stevecrawshaw-bucket",
+    prefix = "pins/",
+    region = "eu-west-2"
+)
+
+# pin_list(board)
+
+# df <- pin_read(board, "ca_la_lookup_tbl")
+# df |> glimpse()
+
+# SPATIAL - Download the GeoParquet file
+
+read_spatial_pin <- function(spatial_pin_name) {
+    pin_path <- pin_download(board, spatial_pin_name)
+    arrow_tbl <- arrow::read_parquet(pin_path, as_data_frame = FALSE)
+    sf_obj <- st_as_sf(as_tibble(arrow_tbl))
+    return(sf_obj)
+}
