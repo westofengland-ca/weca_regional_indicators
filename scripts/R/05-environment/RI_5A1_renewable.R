c39c4873e459362bc468ceeef0b69204442edec1
Steve Crawshaw <murrayjarvis@gmail.com>
2026-04-09 11:02:05 +0100

    add RI_5A1_renewable_capacity_km2 summary and plot

--- scripts/R/05-environment/RI_5A1_renewable.R
+++ scripts/R/05-environment/RI_5A1_renewable.R
@@ -6,13 +6,13 @@
 pacman::p_load(tidyverse, janitor, glue, tidyxl, readxl, sf)
 # connect to the POSTGIS database - imports a connection object "con" into the environment
 source(here::here("scripts", "R", "db_connect.R"))
-source(here::here("scripts", "R", "theme_weca.R"))
+source(here::here("scripts", "R", "_common.R"))
 # spreadsheet https://assets.publishing.service.gov.uk/media/68da76d2c487360cc70c9e9d/Renewable_electricity_by_local_authority_2014_-_2024.xlsx
 
 RI_5A1_current_spreadsheet <- "Renewable_electricity_by_local_authority_2014_-_2024.xlsx"
 period_years <- 10
 # metadata
-RI_51A_contents <-
+RI_5A1_contents <-
     xlsx_cells(
         here::here(
             "data",
@@ -23,8 +23,8 @@ RI_51A_contents <-
     )
 
 # get the start and end year from the contents sheet
-RI_51A_get_year_range <- function(contents) {
-    RI_51A_contents %>%
+RI_5A1_get_year_range <- function(contents) {
+    RI_5A1_contents %>%
         filter(address == "A1") %>%
         pull(character) %>%
         str_extract_all(pattern = "\\d{4}") %>%
@@ -32,14 +32,14 @@ RI_51A_get_year_range <- function(contents) {
         map_int(~ as.integer(.x))
 }
 
-RI_51A_year_range <- RI_51A_get_year_range(RI_51A_contents)
+RI_5A1_year_range <- RI_5A1_get_year_range(RI_5A1_contents)
 
 #'
 #' @param year_range The start and end years in the spreadsheer
 #' @param type The type of renewable energy metric
 #' @return A character vector of names for the sheets of a type
 #'
-RI_51A_get_sheet_names <- function(year_range, type = "generation") {
+RI_5A1_get_sheet_names <- function(year_range, type = "generation") {
     if (type == "generation") {
         sheet_prefix <- "LA - Generation,"
     } else if (type == "capacity") {
@@ -55,9 +55,8 @@ RI_51A_get_sheet_names <- function(year_range, type = "generation") {
     paste(sheet_prefix, year_vec)
 }
 
-
 # extract the relevant columns from the sheet - for this function its just totals, year and LA data
-RI_51A_get_sheet <- function(
+RI_5A1_get_sheet <- function(
     sheet_name,
     path = here::here(
         "data",
@@ -94,7 +93,7 @@ RI_51A_get_sheet <- function(
 }
 
 # get the PV and households columns along with year and LA data
-RI_51A_get_household_pv_sheet <- function(
+RI_5A1_get_household_pv_sheet <- function(
     sheet_name,
     path = here::here(
         "data",
@@ -130,12 +129,12 @@ RI_51A_get_household_pv_sheet <- function(
 
 
 # consolidate the PV and household data from the sites sheets for RI_5A2_domestic_renewable
-RI_51A_compile_household_pv_sheets <- function(RI_51A_contents) {
-    RI_51A_contents %>%
-        RI_51A_get_year_range() |>
-        RI_51A_get_sheet_names(type = "sites") |>
+RI_5A1_compile_household_pv_sheets <- function(RI_5A1_contents) {
+    RI_5A1_contents %>%
+        RI_5A1_get_year_range() |>
+        RI_5A1_get_sheet_names(type = "sites") |>
         map(
-            ~ RI_51A_get_household_pv_sheet(
+            ~ RI_5A1_get_household_pv_sheet(
                 .x,
                 path = here::here(
                     "data",
@@ -148,7 +147,7 @@ RI_51A_compile_household_pv_sheets <- function(RI_51A_contents) {
 }
 
 
-RI_51A_compile_sheets <- function(RI_51A_contents, type = "generation") {
+RI_5A1_compile_sheets <- function(RI_5A1_contents, type = "generation") {
     # capture the units for the respective columns for onward transparency
     ren_total <- function(type) {
         case_when(
@@ -157,11 +156,11 @@ RI_51A_compile_sheets <- function(RI_51A_contents, type = "generation") {
             type == "sites" ~ "sites"
         )
     }
-    RI_51A_contents %>%
-        RI_51A_get_year_range() |>
-        RI_51A_get_sheet_names(type = type) |>
+    RI_5A1_contents %>%
+        RI_5A1_get_year_range() |>
+        RI_5A1_get_sheet_names(type = type) |>
         map(
-            ~ RI_51A_get_sheet(
+            ~ RI_5A1_get_sheet(
                 .x,
                 path = here::here(
                     "data",
@@ -174,18 +173,18 @@ RI_51A_compile_sheets <- function(RI_51A_contents, type = "generation") {
         rename_with(.fn = ~ ren_total(type), .cols = total)
 }
 
-RI_51A_sites_pv_tbl <- RI_51A_compile_household_pv_sheets(RI_51A_contents)
-RI_51A_generation_tbl <- RI_51A_compile_sheets(contents, type = "generation")
-RI_51A_capacity_tbl <- RI_51A_compile_sheets(contents, type = "capacity")
-RI_51A_sites_tbl <- RI_51A_compile_sheets(contents, type = "sites")
+RI_5A1_sites_pv_tbl <- RI_5A1_compile_household_pv_sheets(RI_5A1_contents)
+RI_5A1_generation_tbl <- RI_5A1_compile_sheets(contents, type = "generation")
+RI_5A1_capacity_tbl <- RI_5A1_compile_sheets(contents, type = "capacity")
+RI_5A1_sites_tbl <- RI_5A1_compile_sheets(contents, type = "sites")
 
 # create a single table for all LA's of generation, capacity, households, sites and PV sites
-RI_51A_renewable_tbl <- reduce(
+RI_5A1_renewable_tbl <- reduce(
     list(
-        RI_51A_generation_tbl,
-        RI_51A_capacity_tbl,
-        RI_51A_sites_tbl,
-        RI_51A_sites_pv_tbl
+        RI_5A1_generation_tbl,
+        RI_5A1_capacity_tbl,
+        RI_5A1_sites_tbl,
+        RI_5A1_sites_pv_tbl
     ),
     .f = inner_join
 ) |>
@@ -194,7 +193,7 @@ RI_51A_renewable_tbl <- reduce(
     )
 
 # filter for weca UA's
-RI_51A_weca_renewable_tbl <- RI_51A_renewable_tbl |>
+RI_5A1_weca_renewable_tbl <- RI_5A1_renewable_tbl |>
     filter(
         la_code %in% c("E06000022", "E06000023", "E06000024", "E06000025"),
         year >= (max(year) - period_years + 1)
@@ -211,8 +210,8 @@ lep_area_km2 <- as.integer(
 
 dbDisconnect(con)
 
-# Make the plot for  RI_51A
-RI_51A_renewable_plot <- RI_51A_weca_renewable_tbl |>
+# Make the plot for  RI_5A1
+RI_5A1_renewable_plot <- RI_5A1_weca_renewable_tbl |>
     ggplot(aes(x = year, y = capacity_mw, fill = la_name)) +
     geom_col() +
     scale_fill_manual(values = ua_colors_by_name) +
@@ -227,31 +226,20 @@ RI_51A_renewable_plot <- RI_51A_weca_renewable_tbl |>
     theme_weca() +
     theme(axis.title.y = element_text(angle = 0, vjust = 0.5))
 
-RI_51A_renewable_plot
+RI_5A1_renewable_plot
 
-RI_51A_kpi_vector <-
-    RI_51A_weca_renewable_tbl |>
+RI_5A1_fact_tbl <- RI_5A1_weca_renewable_tbl |>
     group_by(year) |>
     summarise(
-        total_installed_capacity_mwh = sum(capacity_mw),
+        installed_capacity_mw = sum(capacity_mw),
         .groups = "drop"
     ) |>
-    filter(year == max(year) | year == min(year) | year == max(year) - 1) |>
-    pull(total_installed_capacity_mwh) |>
-    set_names(c("first", "previous", "latest"))
+    transmute(
+        period_start = dmy(glue::glue("01-01-{year}")),
+        period_end = dmy(glue::glue("31-12-{year}")),
+        value = installed_capacity_mw / lep_area_km2
+    )
 
-RI_51A_kpi <- RI_51A_kpi_vector["latest"]
-
-RI_51A_latest_previous_change_pc <- (RI_51A_kpi_vector[
-    "latest"
-] /
-    RI_51A_kpi_vector["previous"] -
-    1) *
-    100
-
-RI_51A_latest_first_change_pc <- (RI_51A_kpi_vector[
-    "latest"
-] /
-    RI_51A_kpi_vector["first"] -
-    1) *
-    100
+RI_5A1_fact_tbl |>
+    build_fact("RI_5A1_renewable_capacity_km2") |>
+    save_fact()
