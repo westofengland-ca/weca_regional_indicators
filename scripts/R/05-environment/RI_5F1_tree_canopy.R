550fac7fcd0199cd2923f1389be72dab1d450f3c
Steve Crawshaw <murrayjarvis@gmail.com>
2026-04-13 12:38:28 +0100

    environment indicators done

---
+++ scripts/R/05-environment/RI_5F1_tree_canopy.R
@@ -0,0 +1,54 @@
+pacman::p_load(tidyverse, glue, janitor, here, sf)
+source(here::here("scripts", "R", "_common.R"))
+source(here::here("scripts", "R", "db_connect.R"))
+
+lep_area_m <- as.integer(
+  (st_read(con, query = "SELECT * FROM os.bdline_ua_lep_diss") |>
+    st_area(lep_bound))
+)
+
+# RI_5F1_tree_canopy
+
+RI_5F1_index_year <- 2020
+RI_5F1_nfi_url <- "https://raw.githubusercontent.com/stevecrawshaw/forest-cover-lep/refs/heads/main/data/output/forest_cover_lep.csv"
+
+RI_5F1_tow_url <- "https://raw.githubusercontent.com/stevecrawshaw/forest-cover-lep/refs/heads/main/data/output/tow_cover_lep.csv"
+
+RI_5F1_nfi_tbl <- read_csv(RI_5F1_nfi_url) |>
+  filter(Year >= RI_5F1_index_year) |>
+  mutate(
+    period_start = as.Date(glue("{Year}-01-01")),
+    period_end = as.Date(glue("{Year}-12-31")),
+    value = Hectares
+  )
+
+RI_5F1_index_value <- RI_5F1_nfi_tbl |>
+  filter(Year == RI_5F1_index_year) |>
+  pull(Hectares)
+
+RI_5F1_index_tbl <- RI_5F1_nfi_tbl |>
+  mutate(index = (Hectares - RI_5F1_index_value) * 100 / Hectares)
+
+RI_5F1_plot <- RI_5F1_index_tbl |>
+  ggplot(aes(x = Year, y = index)) +
+  geom_line(linewidth = 1) +
+  scale_y_continuous(
+    labels = scales::label_percent(scale = 1),
+    limits = c(-1, 1)
+  ) +
+  labs(
+    title = "Change in extent of National Forest Inventory Woodland",
+    subtitle = "From 2020 baseline: West of England",
+    x = "Year",
+    y = "%",
+    caption = "Source: Forestry Commission"
+  ) +
+  theme_weca() +
+  theme(axis.title.y = element_text(angle = 0, vjust = 0.5))
+
+RI_5F1_fact_tbl <- RI_5F1_nfi_tbl |>
+  select(period_start, period_end, value)
+
+RI_5F1_fact_tbl |>
+  build_fact(indicator_id = "RI_5F1_tree_canopy") |>
+  save_fact()
---
+++ scripts/R/05-environment/RI_5F4_water_catchment_health.R
@@ -0,0 +1,40 @@
+pacman::p_load(tidyverse, glue, janitor, here)
+source(here::here("scripts", "R", "_common.R"))
+# RI_5F4_water_catchment_health
+
+RI_5F4_water_catchment_health_path <- here::here("data", "raw", "RI_5F4_water_catchment_health_ecological_health_water_bristol_avon_somerset_streams.csv")
+
+RI_5F4_raw_tbl <- read_csv(RI_5F4_water_catchment_health_path) |>
+  clean_names() |> 
+  glimpse()
+
+RI_5F4_fact_tbl <- RI_5F4_raw_tbl |> 
+  transmute(period_start = as.Date(glue("{year}-01-01")),
+         period_end = as.Date(glue("{year}-12-31")),
+         value = (good * 100) /grand_total) |> 
+    glimpse()
+
+RI_5F4_start_year <- min(year(RI_5F4_fact_tbl$period_start))
+RI_5F4_end_year <- max(year(RI_5F4_fact_tbl$period_start))
+  
+RI_5F4_plot <- RI_5F4_fact_tbl |> 
+ transmute(year = year(period_start), value) |> 
+  ggplot(aes(x = year, y = value)) +
+  scale_y_continuous(
+    labels = scales::label_percent(scale = 1),
+    limits = c(0, 100)
+  ) +
+  scale_x_continuous(breaks = seq(RI_5F4_start_year, RI_5F4_end_year, by = 1)) +
+  geom_line(linewidth = 1)  +
+  geom_point(size = 4) +
+  labs(title = "Proportion of Surface Waters in 'Good' Ecological Health",
+       subtitle = "Avon Bristol and Somerset North Streams",
+       x = "Year",
+       y = "%",
+       caption = "Source: Environment Agency") +
+  theme_weca() +
+  theme(axis.title.y = element_text(angle = 0, vjust = 0.5))
+
+RI_5F4_fact_tbl |> 
+  build_fact("RI_5F4_water_catchment_health") |>
+  save_fact()
